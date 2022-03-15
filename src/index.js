d26ff13448babad88290f85e341dbdac52d1c5cb
+ <+>
2022-03-15 16:54:30 -0400

    commit tree 27fbbe8f98e08ab2f3968f0e4ed8f9f3bce6f327

--- src/index.js
+++ src/index.js
@@ -1,7 +1,66 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
+  toggleForm()
+  getToys()
+  NewToyForm()
+});
+
+const NewToyForm = () => {
+  const toyForm = document.querySelector('.add-toy-form')
+  toyForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const newToyName = e.target.name.value
+  const newToyImage = e.target.image.value
+  const newToyObj = {
+    name: newToyName,
+    image: newToyImage, 
+    likes: 0
+  }
+    renderAToy(newToyObj)
+  })
+}
+
+const getToys = () => {
+  fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+  .then(toys => toys.forEach((toy) => renderAToy(toy)))
+}
+
+const renderAToy = (toy) => {
+  const toyCard = document.createElement('div')
+  toyCard.className = "card"
+
+  const toyName = document.createElement('h2')
+  toyName.innerText = toy.name
+
+  const toyLikes = document.createElement('p')
+  toyLikes.innerText = `${toy.likes} Likes`
+
+  const likeBtn = document.createElement('button')
+  likeBtn.innerText = `Like <3`
+  likeBtn.className = 'like-btn'
+  likeBtn.id = toy.id
+
+  likeBtn.addEventListener('click',(e) => {
+    const currentLikesText = e.target.previousSibling.innerText
+    const actualLikes = currentLikesText.split(" ")[0]
+    e.target.previousSibling.innerText = `${parseInt(actualLikes) + 1} Likes`
+    })
+
+  const toyImage = document.createElement('img')
+  toyImage.src = toy.image
+  toyImage.className = "toy-avatar"
+
+  const toyCollection = document.querySelector('#toy-collection')
+  toyCollection.append(toyCard)
+
+  toyCard.append(toyName, toyImage, toyLikes, likeBtn)
+
+}
+
+const toggleForm = () => {
+const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
@@ -11,5 +70,5 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       toyFormContainer.style.display = "none";
     }
-  });
-});
+});  
+}
